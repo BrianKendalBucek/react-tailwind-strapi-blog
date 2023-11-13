f648031c91a7f5c35004bdd11b0a32b29c466940
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-11-13 15:50:03 -0800

    header css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,12 +5,21 @@ const Navbar = () => {
   return (
     <div className='w-screen h-[80px] z-10 bg-white fixed drop-shadow-lg'>
       <div className='flex justify-between items-center w-full h-full md:max-w-[1240px] m-auto'>
+
         <div className='flex items-center'>
           <img src={logo} alt="logo" className='sm:ml-10 ss:ml-10 md:ml-3 opacity-[55%] w-full h-[25px]' />
         </div>
 
+        <div className='flex items-center'>
+          <ul className='hidden md:flex'>
+            <li>Home</li>
+            <li>About</li>
+            <li>Support</li>
+            <li>Platform</li>
+            <li>Pricing</li>
+          </ul>
 
-        <div></div>
+        </div>
         <div></div>
       </div>
 
