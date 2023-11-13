9b2c8ba0f26d3eef4fcbc98d6c3bd1e936bfa854
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-11-13 15:46:02 -0800

    logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import {menu, close, logo} from '../assets';
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className='w-screen h-[80px] z-10 bg-white fixed drop-shadow-lg'>
+      <div className='flex justify-between items-center w-full h-full md:max-w-[1240px] m-auto'>
+        <div className='flex items-center'>
+          <img src={logo} alt="logo" className='sm:ml-10 ss:ml-10 md:ml-3 opacity-[55%] w-full h-[25px]' />
+        </div>
+
+
+        <div></div>
+        <div></div>
+      </div>
+
     </div>
   )
 }
