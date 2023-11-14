44bf420eb410725f0d5b45e9926471c4c7d439a0
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-11-13 16:24:22 -0800

    single blog post appearance

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { Navbar } from "./components"
+import { Navbar, Blogs } from "./components"
 
 
 export default function App() {
   return (
     <div>
       <Navbar />
+      <Blogs />
     </div>
   )
 }
